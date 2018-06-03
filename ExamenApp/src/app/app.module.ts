cc71ccd1b615dad8bcd12ec116e4f71085f5e272
AndresSal <andres.salazar01@epn.edu.ec>
2018-06-02 19:01:10 -0500

    getting started angular material

--- ExamenApp/src/app/app.module.ts
+++ ExamenApp/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]

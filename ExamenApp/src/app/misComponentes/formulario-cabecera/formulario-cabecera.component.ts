17c3bba48c792f879d5998ddce9dd740db21fe1c
AndresSal <andres.salazar01@epn.edu.ec>
2018-06-04 10:35:16 -0500

    formularioCabecera - crear un objeto estudiante con los inputs del formulario y guardarlos en un arreglo

--- ExamenApp/src/app/misComponentes/formulario-cabecera/formulario-cabecera.component.ts
+++ ExamenApp/src/app/misComponentes/formulario-cabecera/formulario-cabecera.component.ts
@@ -1,5 +1,31 @@
 import {Component, Input, OnInit} from '@angular/core';
 
+class Estudiante {
+  primerNombre: string;
+  segundoNombre: string;
+  primerApellido: string;
+  segundoApellido: string;
+  fechaNacimiento: Date;
+  graduado: boolean;
+  semestreActual: number;
+
+  constructor (nombreUno: string,
+               nombreDos: string,
+               apellidoUno: string,
+               apellidoDos: string,
+               fechaNacimiento: Date,
+               graduado: boolean,
+               semestreActual: number) {
+    this.primerNombre = nombreUno;
+    this.segundoNombre = nombreDos;
+    this.primerApellido = apellidoUno;
+    this.segundoApellido = apellidoDos;
+    this.fechaNacimiento = fechaNacimiento;
+    this.graduado = graduado;
+    this.semestreActual = semestreActual;
+  }
+}
+
 @Component({
   selector: 'app-formulario-cabecera',
   templateUrl: './formulario-cabecera.component.html',
@@ -7,23 +33,38 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class FormularioCabeceraComponent implements OnInit {
 
+
   indice: number;
-  nombrePrimero = 'Primer Nombre';
-  nombreSegundo = 'Segundo Nombre';
-  apellidoPrimero = 'Primer Apellido';
-  apellidoSegundo = 'Segundo Apellido';
-  fechaNacimiento = 'Fecha de Nacimiento';
-  semestreActual = 'Semestre Actual';
-  graduado = '¿Es graduado?';
+  nombrePrimero: string;
+  nombreSegundo: string;
+  apellidoPrimero: string;
+  apellidoSegundo: string;
+  fechaNacimiento: Date;
+  semestreActual: number;
+  graduado: boolean;
   goalText = 'My first life goal';
+
   estudiantes = [];
   constructor() { }
   ngOnInit() {
     this.indice = this.estudiantes.length;
   }
   guardarEstudiante() {
-    this.estudiantes.push(this.nombrePrimero);
+    const nuevoEstudiante = new Estudiante(this.nombrePrimero,
+                                this.nombreSegundo,
+                                this.apellidoPrimero,
+                                this.apellidoSegundo,
+                                this.fechaNacimiento,
+                                this.graduado,
+                                this.semestreActual);
+
+    this.estudiantes.push(nuevoEstudiante);
+
     this.nombrePrimero = '';
+    this.nombreSegundo = '';
+    this.apellidoPrimero = '';
+    this.apellidoSegundo = '';
+    this.semestreActual = 0;
     this.indice = this.estudiantes.length;
   }
 }

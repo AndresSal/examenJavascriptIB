4b7b5c28632a86b0fa0920909af7b1afc88336ff
AndresSal <andres.salazar01@epn.edu.ec>
2018-06-05 10:41:51 -0500

    current change

--- ExamenApp/src/app/misComponentes/tabla-principal/tabla-principal.component.ts
+++ ExamenApp/src/app/misComponentes/tabla-principal/tabla-principal.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tabla-principal',
   templateUrl: './tabla-principal.component.html',
   styleUrls: ['./tabla-principal.component.css']
 })
-export class TablaPrincipalComponent implements OnInit {
+export class TablaPrincipalComponent implements OnInit , OnChanges{
 
+  @Input () arregloPrincipal: Array<any>;
+  miColeccion: Array<any>;
   constructor() { }
 
+  ngOnChanges(cambiosRealizados): void {
+    console.log('cambios en la tarjeta => ', cambiosRealizados);
+    this.miColeccion = this.arregloPrincipal;
+  }
   ngOnInit() {
   }
+
+
 
 }
